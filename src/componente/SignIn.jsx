cbb4f5b757d4d2b9ff3c3ee4d7a2dc4ef8e94aee
LiorAharon98 <lior.ah98@gmail.com>
2022-06-18 02:37:42 +0300

    update

--- src/componente/SignIn.jsx
+++ src/componente/SignIn.jsx
@@ -23,7 +23,7 @@ const SignIn = ({ userName, setDisplayNav, setUserName }) => {
           setDisplayNav((prevDisplayNav) => {
             return (prevDisplayNav = true);
           });
-          nav("/burger_menu/menu");
+          nav("/menu");
         }}
       >
         menu
